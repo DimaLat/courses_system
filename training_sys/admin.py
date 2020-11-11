95ddbd90ce463a22da6fbe534938e970a8fce793
DimaLat <dima.latyshionok@gmail.com>
2020-11-11 16:20:32 +0300

    add gitignore, add models to training_sys

--- training_sys/admin.py
+++ training_sys/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Course, Lection, Homework
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Course, CourseAdmin)
+
+
+class LectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'presentation']
+    list_editable = ['presentation']
+
+
+admin.site.register(Lection, LectionAdmin)
+
+
+class HomeworkAdmin(admin.ModelAdmin):
+    list_display = ['txt', 'comment', 'mark']
+    list_editable = ['comment', 'mark']
+
+
+admin.site.register(Homework, HomeworkAdmin)
--- training_sys/models.py
+++ training_sys/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+
+class Teacher(models.Model):
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+
+
+class Student(models.Model):
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    teachers = models.ManyToManyField(Teacher)
+    students = models.ManyToManyField(Student)
+
+
+class Lection(models.Model):
+    title = models.CharField(max_length=80, blank=False)
+    presentation = models.TextField(blank=False)
+
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)
+
+
+class Homework(models.Model):
+    txt = models.CharField(max_length=200, blank=False)
+    comment = models.CharField(max_length=40, blank=True)
+    mark = models.DecimalField(max_digits=3, decimal_places=0, blank=True)
+
+    lection = models.OneToOneField(Lection, on_delete = models.PROTECT)
+    students = models.ManyToManyField(Student)
